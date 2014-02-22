d3d1502b061f4f6b232757236dc3fbc88d1cb111
Jian Li <jianli130@gmail.com>
2014-02-22 14:23:44 -0800

    added main view

---
+++ IOS/GearsGameCenter/GearsGameCenter/TGMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  TGMainViewController.h
+//  GearsGameCenter
+//
+//  Created by LJ-Jian on 2/22/2014.
+//  Copyright (c) 2014 Gears. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TGMainViewController : UITableViewController
+
+@end
