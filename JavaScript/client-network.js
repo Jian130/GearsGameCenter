5351f98ddae48fa974b86cdd3653eeccebaf0ca4
gjcchan <c_george_c@hotmail.com>
2014-02-22 10:05:07 -0800

    initial creation of file
    first commit test and creating necessary file

---
+++ JavaScript/client-network.js
@@ -0,0 +1,16 @@
+<script>
+	var connection = new WebSocket('',soap, xmpp);
+
+	//connection error handling
+	connection.onerror = function(error) {
+
+	}
+	//initial connection sequence
+	connection.onopen = function() {
+
+	}
+	connection.onmessage = function(e) {
+		//insert data here
+	}
+
+</script>
