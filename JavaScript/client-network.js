c147306af224023dd33786062671d7e1fcfc3d5a
gjcchan <c_george_c@hotmail.com>
2014-02-22 11:33:28 -0800

    first working client network layer

--- JavaScript/client-network.js
+++ JavaScript/client-network.js
@@ -1,16 +1,73 @@
-<script>
-	var connection = new WebSocket('',soap, xmpp);
+
+//client network layer
+
+//can send data in binary
+	//json object
+
+	var gameStateObject;
+	var recievedObject;
+	var wsPort;
+
+	//init connection
+	var connection;
+	//sendOut(gameStateObject);
+	//to close connection connection.close();
+	window.onload = function() {
+		console.log("loading!");
+		//window.alert("loading!");
+		//check preconditions for web socket support
+		if (window.MozWebSocket)
+	    {
+	        console.log('using MozillaWebSocket');
+	        window.WebSocket = window.MozWebSocket;
+	    }
+	    else if (!window.WebSocket)
+	    {
+	        console.log('browser does not support websockets!');
+	        return;
+	    }
+		gameStateObject = {message1: "test", message2:"test2"};
+		wsPort = (window.location.port.toString() === "" ? "" : ":"+window.location.port);
+		/*if (wsUri.value === "") {
+        	wsUri.value = "ws://" + window.location.hostname.replace("www", "echo") + wsPort;
+    	}*/
+		connection = new WebSocket('ws://echo.websocket.org');//,soap, xmpp);
+		connection.onopen = function(event) { onConnection() };
+		connection.onerror = function(errror) { connectionError(error) };
+		connection.onmessage = function(object) { recieveObject(object)};
+		connection.onclose = function(event) { onCloseEvent() };
+
+		//sendOut(gameStateObject);		
+	}
 
 	//connection error handling
-	connection.onerror = function(error) {
-
+	function connectionError(error) {
+		console.log("connection error: " + error);
+		alert(error);
+		document.getElementById("#yolo").innerHTML = error;
 	}
 	//initial connection sequence
-	connection.onopen = function() {
-
+	function onConnection() {
+		console.log("connected");
+		sendOut(gameStateObject);
 	}
-	connection.onmessage = function(e) {
-		//insert data here
+	function onCloseEvent() {
+		console.log("closing");
+	}
+	function recieveObject(input) {
+		//convert JSON
+		console.log(input);
+		recievedObject = JSON.parse(input.data);
+		console.log(recievedObject);
+		//other data handling here
+	}
+	function sendOut(object) {
+		if(connection.readyState == 1)
+		{
+			connection.send(JSON.stringify(object));
+		} else {
+			console.log("connection not ready!");
+		}
 	}
 
-</script>
+	//window.addEventListener("load", echoHandlePageLoad, false);
