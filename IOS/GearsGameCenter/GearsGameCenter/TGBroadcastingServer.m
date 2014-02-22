54706e3f3ae0724377c92dd6a2f491bd51881a9d
Jian Li <jianli130@gmail.com>
2014-02-22 10:31:17 -0800

    server classes added

---
+++ IOS/GearsGameCenter/GearsGameCenter/TGBroadcastingServer.m
@@ -0,0 +1,13 @@
+//
+//  TGBroadcastingServer.m
+//  GearsGameCenter
+//
+//  Created by LJ-Jian on 2/22/2014.
+//  Copyright (c) 2014 Gears. All rights reserved.
+//
+
+#import "TGBroadcastingServer.h"
+
+@implementation TGBroadcastingServer
+
+@end
---
+++ IOS/GearsGameCenter/GearsGameCenter/TGWebServer.h
@@ -0,0 +1,13 @@
+//
+//  TGWebServer.h
+//  GearsGameCenter
+//
+//  Created by LJ-Jian on 2/22/2014.
+//  Copyright (c) 2014 Gears. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TGWebServer : NSObject
+
+@end
