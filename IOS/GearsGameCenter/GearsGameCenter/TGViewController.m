ad5bd5f81b4dbbc702792bec44749dd83f92919a
peterrau <yichengrau@gmail.com>
2014-02-22 12:08:42 -0800

    ui

--- IOS/GearsGameCenter/GearsGameCenter/TGViewController.m
+++ IOS/GearsGameCenter/GearsGameCenter/TGViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "TGViewController.h"
+#import <MessageUI/MessageUI.h>
 
-@interface TGViewController ()
+@interface TGViewController () <MFMessageComposeViewControllerDelegate>
+
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *shareButton;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -19,7 +23,10 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    //start web server
+    NSString *fullURL = @"http://192.168.16.14/Games/Maze/index.html";
+    NSURL *url = [NSURL URLWithString:fullURL];
+    NSURLRequest *requestObj = [NSURLRequest requestWithURL:url];
+    [_webView loadRequest:requestObj];
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,6 +35,47 @@
     // Dispose of any resources that can be recreated.
     
     
+}
+
+- (IBAction)shareTapped:(id)sender {
+    if(![MFMessageComposeViewController canSendText]) {
+        UIAlertView *warningAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Your device doesn't support SMS!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+        [warningAlert show];
+        return;
+    }
+    
+    NSString *gameLink = @"http://192.168.16.14/Games/Maze/index.html";
+    NSString *message = [@"Please join the Maze game: " stringByAppendingString:gameLink];
+    
+    MFMessageComposeViewController *messageController = [[MFMessageComposeViewController alloc] init];
+    messageController.messageComposeDelegate = self;
+    [messageController setBody:message];
+    
+    // Present message view controller on screen
+    [self presentViewController:messageController animated:YES completion:nil];
+}
+
+- (void)messageComposeViewController:(MFMessageComposeViewController *)controller didFinishWithResult:(MessageComposeResult) result
+{
+    switch (result) {
+        case MessageComposeResultCancelled:
+            break;
+            
+        case MessageComposeResultFailed:
+        {
+            UIAlertView *warningAlert = [[UIAlertView alloc] initWithTitle:@"Error" message:@"Failed to send SMS!" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+            [warningAlert show];
+            break;
+        }
+            
+        case MessageComposeResultSent:
+            break;
+            
+        default:
+            break;
+    }
+    
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 @end
