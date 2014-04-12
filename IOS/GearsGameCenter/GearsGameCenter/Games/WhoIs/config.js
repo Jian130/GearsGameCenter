c78f7e478cb70f436a49dc43ba086aabb5178216
Greg Zhou <bottlezz2002@gmail.com>
2014-04-12 13:52:18 -0700

    sync

--- IOS/GearsGameCenter/GearsGameCenter/Games/WhoIs/config.js
+++ IOS/GearsGameCenter/GearsGameCenter/Games/WhoIs/config.js
@@ -5,8 +5,8 @@ var ADAPT_CONFIG = {
 		    '0px    to 760px  = 720.css',
 		    '760px  to 980px  = 720.css',
 		    '980px  to 1280px = 980.css',
-		    '1280px to 1600px = 980.css',
-		    '1600px to 1920px = 980.css',
-		    '1920px           = 980.css'
+		    '1280px to 1600px = 1280.css',
+		    '1600px to 1920px = 1280.css',
+		    '1920px           = 1280.css'
 		  ]
 	};
